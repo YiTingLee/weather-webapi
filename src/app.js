6fda0d71ac66934c158b9640663dd0fddf9c9178
Eric Lee <ranye0719@gmail.com>
2019-03-02 12:30:50 +0800

    basic html

--- src/app.js
+++ src/app.js
@@ -1,11 +1,10 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
+const publicDirectoryPath = path.join(__dirname, '../public');
 
-app.get('', (req, res) => {
-  res.send('Hello express!');
-})
-
+app.use(express.static(publicDirectoryPath));
 
 app.listen(3000, () => {
   console.log('Server is up on port 3000.');
