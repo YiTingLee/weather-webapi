2766d3eda09d8f88ea1041679f64e6ac9fa178db
Eric Lee <ranye0719@gmail.com>
2019-03-02 12:11:29 +0800

    init express

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('', (req, res) => {
+  res.send('Hello express!');
+})
+
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000.');
+});
