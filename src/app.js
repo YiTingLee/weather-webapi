dd6d94884e2b5485afa2d7b6764f2b09e9e44eb7
Eric Lee <ranye0719@gmail.com>
2019-03-03 11:09:53 +0800

    a basic weather api has been added

--- src/app.js
+++ src/app.js
@@ -6,6 +6,17 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
+app.get('/api/weather', (req, res) => {
+  if (!req.query.apiKey || !req.query.apiKey2 || !req.query.address) {
+    res.send({
+      error: 'You must provide some arguments.'
+    })
+  }
+  res.send({
+    location: req.query.address
+  })
+})
+
 app.listen(3000, () => {
   console.log('Server is up on port 3000.');
 });
